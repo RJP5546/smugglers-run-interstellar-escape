cdb9182b06f9b28a3fbab760954de2bb93856029
RJP5546 <111258984+RJP5546@users.noreply.github.com>
2024-05-20 22:17:41 -0500

    Added Black void to world transition

--- SmugglersRun/Assets/Scripts/UI/ScoreManager.cs
+++ SmugglersRun/Assets/Scripts/UI/ScoreManager.cs
@@ -8,6 +8,7 @@ public class ScoreManager : MonoBehaviour
 {
 
     [SerializeField] private GameManager gm;
+    [SerializeField] private GameObject WarpTunnel;
     [SerializeField] private Camera gameCamera;
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private TextMeshProUGUI scoreTextEnd;
@@ -19,13 +20,21 @@ public class ScoreManager : MonoBehaviour
     private float highScore;
     private float originalCamFOV;
 
+    private Material WarpTunnelMaterial;
+    private Color WarpTunnelAlpha;
+
     private void Start()
     {
         scoreVal = 0;
         highScore = PlayerPrefs.GetInt("HighScore");
         highScoreText.text = "High Score: " + highScore;
         originalCamFOV = gameCamera.fieldOfView;
-        
+
+        WarpTunnelMaterial = WarpTunnel.GetComponent<Renderer>().material;
+        WarpTunnelAlpha = WarpTunnelMaterial.color;
+        WarpTunnelAlpha.a = 0f;
+        WarpTunnelMaterial.color = WarpTunnelAlpha;
+
     }
 
     void FixedUpdate()
@@ -54,7 +63,9 @@ public class ScoreManager : MonoBehaviour
 
         while (timeElapsed < 3f)
         {
-            gameCamera.fieldOfView = Mathf.Lerp(originalCamFOV, 170, timeElapsed / 3f);
+            gameCamera.fieldOfView = Mathf.Lerp(originalCamFOV, 168, timeElapsed / 3f);
+            WarpTunnelAlpha.a = Mathf.Lerp(0, 1, timeElapsed / 3f);
+            WarpTunnelMaterial.color = WarpTunnelAlpha;
             timeElapsed += Time.deltaTime;
 
             yield return null;
@@ -69,7 +80,9 @@ public class ScoreManager : MonoBehaviour
 
         while (timeElapsed < 3f)
         {
-            gameCamera.fieldOfView = Mathf.Lerp(170, originalCamFOV, timeElapsed / 3f);
+            gameCamera.fieldOfView = Mathf.Lerp(168, originalCamFOV, timeElapsed / 3f);
+            WarpTunnelAlpha.a = Mathf.Lerp(1, 0, timeElapsed / 3f);
+            WarpTunnelMaterial.color = WarpTunnelAlpha;
             timeElapsed += Time.deltaTime;
 
             yield return null;
